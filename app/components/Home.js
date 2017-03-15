91d486765d8ea7f708e5d1315c11f4d0419a7cd1
Thomas Brüggemann <mail@thomasbrueggemann.com>
2017-03-15 22:32:35 +0100

    landing page

--- app/components/Home.js
+++ app/components/Home.js
@@ -2,45 +2,98 @@ import React from "react";
 import { Link } from "react-router";
 
 class Home extends React.Component {
-    // COMPONENT DID MOUNT
-    componentDidMount() {
-        var b = document.getElementsByTagName("body")[0];
-        b.className = "launcher";
-    }
-
-    // COMPONENT WILL UNMOUNT
-    componentWillUnmount() {
-        var b = document.getElementsByTagName("body")[0];
-        b.className = "";
-    }
-
     // RENDER
     render() {
         return (
-            <div className="row">
-                <div className="column">
-                    <img src="/img/iphone.png" height="750px" />
+            <div>
+                <div className="row">
+                    <div className="column">
+                        <div className="marvel-device iphone6 silver">
+                            <div className="top-bar" />
+                            <div className="sleep" />
+                            <div className="volume" />
+                            <div className="camera" />
+                            <div className="sensor" />
+                            <div className="speaker" />
+                            <div className="screen">
+                                <img src="/img/screen.png" />
+                            </div>
+                            <div className="home" />
+                            <div className="bottom-bar" />
+                        </div>
+                    </div>
+                    <div className="column">
+
+                        <div className="hero-text">
+                            <center>
+                                <img
+                                    src="/img/android-chrome-512x512.png"
+                                    width="175"
+                                />
+                                <h1>Trevvio</h1>
+
+                                <h4>
+                                    Share your location with your friends, instantly.
+                                </h4>
+
+                                <div className="border-line" />
+
+                                <ul>
+                                    <li>
+                                        <i className="fa fa-users" />
+                                        {" "}
+                                        You decice who you share your location with
+                                    </li>
+                                    <li>
+                                        <i className="fa fa-clock-o" />
+                                        {" "}
+                                        You can stop sharing at any time
+                                    </li>
+                                    <li>
+                                        <i className="fa fa-lock" />
+                                        {" "}
+                                        We
+                                        {" "}
+                                        <u>don't</u>
+                                        {" "}
+                                        collect or store any of your data and we're particularly proud of that!
+                                    </li>
+                                </ul>
+
+                                <div className="border-line" />
+                            </center>
+
+                            <div className="row">
+                                <div className="column">
+                                    <a href="https://apple.com" target="_blank">
+                                        <img
+                                            src="/img/appstore.svg"
+                                            height="80"
+                                        />
+                                    </a>
+                                </div>
+                                <div className="column">
+                                    <a
+                                        href="https://google.com"
+                                        target="_blank"
+                                    >
+                                        <img
+                                            src="/img/playstore.svg"
+                                            height="80"
+                                        />
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <div className="column column-hero-text">
-
-                    <div className="hero-text">
-                        <center>
-                            <img
-                                src="/img/android-chrome-512x512.png"
-                                width="175"
-                            />
-                            <h1>Trevvio</h1>
-
-                            <p>
-                                Share your position with your friends instantly.
-                            </p>
-
-                            <Link to={"/imprint"}>Imprint</Link>
-                            {" "}
-                            ·
-                            {" "}
-                            <Link to={"/privacy"}>Privacy Policy</Link>
-                        </center>
+                <div className="row footer-row">
+                    <div className="column footer-column">
+                        <a href="/imprint">Imprint</a>
+                        {" "}
+                        ·
+                        {" "}
+                        <a href="/privacy">Privacy Policy</a> · © Trevvio
                     </div>
                 </div>
             </div>
